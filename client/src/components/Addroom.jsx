0daae6ed34b2a6ad81e55912ecceb79e1928d5c1
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2023-12-18 10:11:30 +0530

    Create Addroom.jsx

---
+++ client/src/components/Addroom.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Addroom() {
+  return (
+    <div className="App">
+        <h1>Input room details</h1>
+      
+    </div>
+  );
+}
+
+export default Addroom;
