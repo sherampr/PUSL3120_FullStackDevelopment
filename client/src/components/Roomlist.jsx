abe43e7e61f371a98bf00e581008127146510bb8
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2023-12-18 10:24:19 +0530

    Create Roomlist.jsx
    create a components to input room details

---
+++ client/src/components/Roomlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Roomlist() {
+  return (
+    <div className="App">
+        <h1>Display all rooms</h1>
+      
+    </div>
+  );
+}
+
+export default Roomlist;
