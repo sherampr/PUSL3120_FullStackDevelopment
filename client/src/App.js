96924a761af4be0d9f652994cae071978c029494
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2023-12-18 12:51:07 +0530

    changes in routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 
 import Roomlist from './components/Roomlist';
-import edit from './components/Room_edit';
-import {BrowserRouter as Router,Route}from 'react-router-dom'
+import editroom from './components/Room_edit';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Addroom from './components/Addroom';
-import { Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Addroom}/>
-          <Route path="/editpage" component={edit}/>
-          <Route path="/list" component={Roomlist}/>
-
-
-        </Switch>
-
-      </Router>
 
   
 
--- client/src/components/Room_edit.jsx
+++ client/src/components/Room_edit.jsx
@@ -1,7 +1,7 @@
 import React from "react";
  
 
-export default function edit(){
+export default function editroom(){
 
    <div>
 
