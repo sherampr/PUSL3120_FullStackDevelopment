391b660129c54633beacaccda8da7f07a52f5c80
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2023-12-18 12:25:35 +0530

    set routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Roomlist from './components/Roomlist';
 import edit from './components/Room_edit';
+import {BrowserRouter as Router,Route}from 'react-router-dom'
+import Addroom from './components/Addroom';
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Addroom}/>
+          <Route path="/editpage" component={edit}/>
+          <Route path="/list" component={Roomlist}/>
+
+
+        </Switch>
+
+      </Router>
+
+  
+
       
     </div>
   );
