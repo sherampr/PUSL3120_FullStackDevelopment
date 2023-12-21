27ae84d712a89ffab3f64d98884431a7c03d5635
Chenuka Sanvidu <113669974+Chenuka1@users.noreply.github.com>
2023-12-21 20:49:04 +0530

    update booking form

--- client/src/pages/Booking_form.jsx
+++ client/src/pages/Booking_form.jsx
@@ -1,44 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-function bookingForm(){
-    return(
-        <div>
+function BookingForm() {
+  const [checkinDate, setCheckinDate] = useState(null);
+  const [checkoutDate, setCheckoutDate] = useState(null);
 
-            
-            <form method="POST">
-                <table>
-                    <tr>
-                        <td>Name</td>
-                        <td><input type="text" placeholder="Enter name"/></td>
-                    </tr>
-                    <tr>
-                        <td>Email</td>
-                        <td><input type="text" placeholder="Enter email address"/></td>
-                    </tr>
-                    <tr>
-                        <td>Select chekin</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>Select checkout</td>
-                        <td></td>
-                    </tr>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
 
-                </table>
-
-
-            </form>
-
-
-
-
-        </div>
-
-
-
-    )
-
-
-
+  return (
+    <div className="booking-form-container">
+      <form onSubmit={handleSubmit}>
+        <table>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>
+                <input type="text" placeholder="Enter name" />
+              </td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>
+                <input type="text" placeholder="Enter email address" />
+              </td>
+            </tr>
+            <tr>
+              <td>Select check-in</td>
+              <td>
+                <DatePicker
+                  selected={checkinDate}
+                  onChange={(date) => setCheckinDate(date)}
+                  placeholderText="Select check-in date"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Select check-out</td>
+              <td>
+                <DatePicker
+                  selected={checkoutDate}
+                  onChange={(date) => setCheckoutDate(date)}
+                  placeholderText="Select check-out date"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
-export default bookingForm;
+
+export default BookingForm;
