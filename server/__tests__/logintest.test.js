d7a76a701b0261b615d53b355e0dc13184dbfda1
sasunnethmina <sasunnethmina@gmail.com>
2024-01-04 23:01:30 +0530

    test

--- server/__tests__/logintest.test.js
+++ server/__tests__/logintest.test.js
@@ -1,22 +1,25 @@
-require("dotenv").config(); // Make sure this is at the top of your file
+require("dotenv").config();
 
 const request = require("supertest");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { User } = require("../models/userModel");
 const app = require("../index");
-const User = require("../models/userModel");
-const bcrypt = require("bcrypt"); // Make sure bcrypt is required
 
-describe("Authentication API", () => {
+describe("Authentication", () => {
   let userData = {
     firstName: "Test",
     lastName: "User",
-    email: "testuser@example.com",
+    email: "testthis@example.com",
     password: "Password@123",
+    confirmPassword: "Password@123",
     phone: "1234567890",
   };
 
   beforeAll(async () => {
-    await mongoose.connect(process.env.TEST_MONG_URI, {
+    jest.setTimeout(100000);
+
+    await mongoose.connect(process.env.MONG_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -24,21 +27,20 @@ describe("Authentication API", () => {
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(userData.password, salt);
     await User.create({ ...userData, password: hashPassword });
-  });
+  }, 100000);
 
   afterAll(async () => {
-    await User.findOneAndDelete({ email: userData.email });
+    await User.deleteOne({ email: userData.email });
     await mongoose.disconnect();
   });
 
   test("User registration", async () => {
-    const response = await request(app).post("/api/users").send({
-      firstName: userData.firstName,
-      lastName: userData.lastName,
-      email: userData.email,
-      password: userData.password,
-      phone: userData.phone,
-    });
+    const newUser = {
+      ...userData,
+      email: "newtestuser@example.com",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
 
     expect(response.statusCode).toBe(201);
     expect(response.body).toHaveProperty("token");
