19ec853670550187129692634212bb471bd260fb
mrakeelz <akeelahamadaaa100@gmail.com>
2024-01-06 23:00:35 +0530

    Menu page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Rooms from './pages/Rooms';
+import Footer from './components/Footer';
+import RoomDetails from './pages/RoomDetails';
+import NewRoom from './staff/pages/NewRoom';
+import RoomList from './staff/pages/RoomList';
+import UpdateRoom from './staff/pages/UpdateRoom';
+// import { BookingProvider } from './contexts/BookingContext';
+import BookingConfirmation  from './pages/BookingConfirmation';
+import StaffNavbar from './staff/components/StaffNavbar';
+import AddMenu from './pages/menu';
+import { Menu } from '@mui/icons-material';
+import ViewMenu from './pages/view_menu';
+import TableReservationForm from './pages/book';
+import ReservationsList from './pages/ReservationsList';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+ 
+     <NavbarSwitcher/>
+        <div className='pages'>
+          <Routes>
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            <Route
+        path='/rooms'
+        element={<Rooms />}
+      />
+       <Route path="/room-details/:id" element={<RoomDetails/>} />
+
+       <Route path="/staff/newroom" element={<NewRoom/>} />
+       <Route path="/staff/menu" element={<AddMenu/>} />
+       <Route path="/reservation" element={<TableReservationForm/>} />
+       <Route path="/staff/viewbook" element={<ReservationsList/>} />
+
+
+       <Route path="/staff/updateroom/:id" element={<UpdateRoom/>} />
+       <Route path="/staff" element={<RoomList/>} />
+       <Route path="/menu" element={<ViewMenu/>} />
+
+        <Route path="/booking-confirmation" element={<BookingConfirmation />} />
+          </Routes>
+        </div>
+        
+<Footer/>
+
+     </BrowserRouter>
     </div>
   );
+}
+
+function NavbarSwitcher() {
+  const location = useLocation();
+
+  // Check if the current path is one of the staff paths
+  const isStaffPage = location.pathname.includes('/staff'); // Modify this as per your route structure
+
+  // Return the appropriate navbar
+  return isStaffPage ? <StaffNavbar /> : <Navbar />;
 }
 
 export default App;
